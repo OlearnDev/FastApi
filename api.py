f21183c8ad614d7624c2e0604ba099ee867894be
Olearndev <mis.olivier@gmail.com>
2024-10-10 19:36:02 -0400

    Test component

--- api.py
+++ api.py
@@ -13,3 +13,13 @@ async def hello_world():
 #   uvicorn.run(app, host="127.0.0.1", port=8000)
     #app.run(debug=True)
 
+@app.get("/Component/{component_id}") #path parameter
+async def get_component(component_id:int):
+	# operations
+	return {"component_id" : component_id}
+
+@app.get("/component/") #path parameter
+async def read_component(number:int, text:str):
+	return {"number" : number, "text": text}
+
+#http://127.0.0.1:8000/component/?number=12&text=Le%20contenu%20du%20texte
